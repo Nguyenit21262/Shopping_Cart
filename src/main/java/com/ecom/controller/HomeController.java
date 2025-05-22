64d1569f862e2386b3bc98462ea46574a575c194
Nguyenit21262 <tranvannguyen24012003@gmail.com>
2025-05-22 19:25:30 +0700

    registration & Login(Spring security)

--- src/main/java/com/ecom/controller/HomeController.java
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -1,36 +1,79 @@
 package com.ecom.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ecom.model.UserDtls;
+import com.ecom.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
-	@GetMapping("/Index")
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
-	
-	@GetMapping("/login")
+
+	@GetMapping("/signin")
 	public String login() {
 		return "login";
 	}
-	
+
 	@GetMapping("/register")
 	public String register() {
 		return "register";
 	}
-	
+
 	@GetMapping("/products")
-		public String products() {
+	public String products() {
 		return "product";
 	}
-	
+
 	@GetMapping("/product")
 	public String viewProduct() {
 		return "view_product";
- 	}
-	
+	}
+
+	@PostMapping("/saveUser")
+	public String saveUser(@ModelAttribute UserDtls user, @RequestParam("img") MultipartFile file, HttpSession session) throws IOException {
+		String imageName = file.isEmpty() ? "default.jpg" : file.getOriginalFilename();
+		user.setProfileImage(imageName);
+		UserDtls saveUser = userService.saveUser(user);
+
+		if (!ObjectUtils.isEmpty(saveUser)) {
+			if (!file.isEmpty()) {
+				
+				File saveFile = new ClassPathResource("static/img").getFile();
+				Path path = Path.of(saveFile.getAbsolutePath() + File.separator + "category_img" + File.separator
+						+ file.getOriginalFilename());
+			//	System.out.print(path);
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+				
+			}
+			session.setAttribute("succMsg", "Register successfully");
+
+		} else {
+			session.setAttribute("errorMsg","Something wrong on server ");
+		}
+		
+		return "redirect:/register";
+	}
 }
