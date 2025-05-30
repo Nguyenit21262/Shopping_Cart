8f8144d250045c0e11e9396ca74b504748f34b72
Nguyenit21262 <tranvannguyen24012003@gmail.com>
2025-05-31 00:23:19 +0700

    uodate UI

--- src/main/java/com/ecom/controller/HomeController.java
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -88,7 +88,7 @@ public class HomeController {
 	@GetMapping("/products")
 	public String products(Model m,@RequestParam(value = "category", defaultValue = "") String category,
 			@RequestParam(name = "pageNo", defaultValue = "0") Integer pageNo,
-			@RequestParam(name = "pageSize", defaultValue = "9") Integer pageSize) {
+			@RequestParam(name = "pageSize", defaultValue = "8") Integer pageSize) {
 		//System.out.println("category =" + category);
 		List<Category> categories = categoryService.getAllActiveCategory();
 		m.addAttribute("paramValue", category);
